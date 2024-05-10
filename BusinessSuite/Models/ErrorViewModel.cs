1a8af99a8d0a9083258d35e46c6fa58ee0c99611
SHREE <SHREE@DESKTOP-NOQE41E>
2024-05-10 13:23:32 +0530

    Add project files.

---
+++ BusinessSuite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessSuite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
