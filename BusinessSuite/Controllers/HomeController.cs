6fefc08cfc2df606e43e6758154b8489a26af8d8
SHREE <SHREE@DESKTOP-NOQE41E>
2024-05-10 14:04:56 +0530

    Added Identity Framework pages.

--- BusinessSuite/Controllers/HomeController.cs
+++ BusinessSuite/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using BusinessSuite.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BusinessSuite.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
