5cbed600c8bdfa89a9665123da9341648336fd58
SHREE <SHREE@DESKTOP-NOQE41E>
2024-07-19 13:01:45 +0530

    campaign date added.

--- BusinessSuite/Migrations/20240719073109_crm2.Designer.cs
+++ BusinessSuite/Migrations/20240719073109_crm2.Designer.cs
@@ -4,6 +4,7 @@ using BusinessSuite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BusinessSuite.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240719073109_crm2")]
+    partial class crm2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BusinessSuite/Models/Campaigns.cs
+++ BusinessSuite/Models/Campaigns.cs
@@ -8,6 +8,7 @@ namespace BusinessSuite.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
         public ICollection<Campaigns_Customers> CampaignCustomers { get; set; }
 
         public ICollection<Campaigns_Marketings> MarketingCampaigns { get; set; }
